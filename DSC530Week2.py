d71e37c0ca9554a3faec101948b272de4b811758
ljquandt <89957231+ljquandt@users.noreply.github.com>
2021-12-12 19:01:00 -0600

    Create DSC530Week2.py

---
+++ DSC530Week2.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec  6 11:40:26 2021
+
+@author: logan
+"""
+
+print("Hello World")
+a = 2 + 2
+print(a)
+b= 4 * 2
+print(b)
+c = 4 / 2
+print(c)
+d = 'apple' + 'bees'
+print(d)
+b_list = ['a','b','c','d']
+print(b_list)
+b_list.append('e')
+print(b_list)
+t1= ('a', 'b', 'c', 'd')
+print(type(t1))
